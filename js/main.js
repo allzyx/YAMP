2855e29dde4a57dc91bab16fe533485f6191e021
allzyx <allzyx@proton.me>
2024-06-15 19:10:46 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openCityForm() {
+  document.getElementById("city").style.display = "block";
+}
+
+function closeCityForm() {
+  document.getElementById("city").style.display = "none";
+}
